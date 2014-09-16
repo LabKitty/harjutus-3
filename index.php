5bda5f97b3d41652754ac899d5b05133d93a3bdd
LabKitty <Shepard1>
2014-09-16 09:26:15 +0300

    Add sample variables

--- index.php
+++ index.php
@@ -6,14 +6,31 @@
 </head>
 
 <body>
-	 <h2>Muutujad</h2>
-	<?php
-		$variable = 10;
-		echo $variable;
-		// Lisan veebilehitsejas parema loetavuse saamiseks reamurdmise elemendi:
-		echo "<br>";
-		$variable = 20;
-		echo $variable;
-	?>
+	<h2>Muutujad</h2>
+		<?php
+			$variable = 10;
+			echo $variable;
+			
+			// Lisan veebilehitsejas parema loetavuse saamiseks reamurdmise elemendi:
+			echo "<br>";
+			$variable = 20;
+			echo $variable;
+			echo "<br>";
+			
+			// Selles näites on "Hello world!" string, mis koosneb 12 tähemärgist (ka tühik ning hüüumärk on tähemärgid):
+			echo "Hello world!";
+		?>
+	
+	<h2>Stringid</h2>
+		<?php
+			echo "let's go";
+			echo "<br>";
+			
+				$variable = 24;
+			echo $variable;
+				$variable = 26;
+			echo $variable;
+		?>
+
 </body>
 </html>
