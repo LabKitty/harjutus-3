599c49bd4446dee7ac411ba170fa788f2b9ac40e
LabKitty <Shepard1>
2014-09-16 09:17:07 +0300

    Add sample variables'

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>harjutus-3 - Andmetüübid</title>
+</head>
+
+<body>
+	 <h2>Muutujad</h2>
+	<?php
+		$variable = 10;
+		echo $variable;
+		// Lisan veebilehitsejas parema loetavuse saamiseks reamurdmise elemendi:
+		echo "<br>";
+		$variable = 20;
+    echo $variable;
+	?>
+</body>
+</html>
