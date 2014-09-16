d71618c5f2549d263fb3820810aa20d5494e63af
LabKitty <Shepard1>
2014-09-16 09:39:19 +0300

    Add sample content echo functions

--- index.php
+++ index.php
@@ -25,12 +25,30 @@
 		<?php
 			echo "let's go";
 			echo "<br>";
-			
+
 				$variable = 24;
 			echo $variable;
 				$variable = 26;
 			echo $variable;
 		?>
-
+		
+	<h2>Nimekirja väljastamine muutujate ja stringide abil</h2>
+		<?php
+			$student1 = "Merili";
+			$student2 = "Maile";
+			$student3 = "Triin";
+			$student4 = "Marge";
+			$student5 = "Marko";
+		?>
+		
+		<ul>
+		<?php
+			echo "<li>" . $student1 . "</li>";
+			echo "<li>" . $student2 . "</li>";
+			echo "<li>" . $student3 . "</li>";
+			echo "<li>" . $student4 . "</li>";
+			echo "<li>" . $student5 . "</li>";
+		?>
+		</ul>
 </body>
 </html>
